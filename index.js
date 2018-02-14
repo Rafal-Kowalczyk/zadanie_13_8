3f5689d58cf97276b73f5676b6a4dc8de8d1f3cf
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-02-14 23:27:55 +0100

    Add code index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-﻿
+﻿var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/index') {
+    	fs.readFile('./index.html', 'utf-8', function(err, html) {
+	        response.write('<h1>index.html</h1>');
+	        response.write(html);
+			 response.end();
+		});	 
+    } else {
+        fs.readFile("./statusCode404.jpg", "binary", function(error, file) {
+	        response.writeHead(200, {"Content-Type": "image/jpg"});
+	        response.write(file, "binary");
+	        response.end();
+	    });
+	}
+ });
+
+server.listen(8084);
